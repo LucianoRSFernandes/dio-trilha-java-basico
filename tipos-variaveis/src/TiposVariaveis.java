572f8be893bead7e7f768999b970982e76d2ad2f
LucianoRSFernandes <luciano_r_s_fernandes@yahoo.com.br>
2023-04-02 09:00:33 -0300

    Exemplo de tipos de variaveis Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //estudem tipos primitivos
+        //estudem a classe String que representa texo na aplicação
+
+        String meuNome = "Gleyson"
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALORDEPI = 3.14;
     }
 }
